8537e86c803e317912afb6cda18e8692b98629d2
追梦人 <128029849+lndsqhj@users.noreply.github.com>
2025-02-05 17:57:26 +0800

    Update py_rou.py

--- py/py_rou.py
+++ py/py_rou.py
@@ -13,7 +13,7 @@ class Spider(Spider):
         return "Rou"
 
     def init(self, extend):
-        self.home_url = 'https://rou.video'
+        self.home_url = 'https://rouvz1.xyz/'
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"}
 
